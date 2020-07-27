845669fde4159ffe65001cc984db2459380af80c
Josh Gum <revgum@gmail.com>
2020-07-27 10:01:40 -0700

    adds uber baseweb

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+import { StatefulInput } from 'baseui/input';
 
-const Application: React.SFC<{}> = () => <h1>Applications too</h1>;
+const engine = new Styletron();
+
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
+
+const Application: React.SFC<{}> = () => (
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <Centered>
+        <StatefulInput />
+      </Centered>
+    </BaseProvider>
+  </StyletronProvider>
+);
 
 render(<Application />, document.getElementById('root'));
