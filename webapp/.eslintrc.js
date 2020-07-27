0bc12ff52b82592fa75053587ef5d63023c20da3
Josh Gum <revgum@gmail.com>
2020-07-27 09:37:45 -0700

    adds prettier and linting

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -4,31 +4,29 @@ module.exports = {
     es6: true,
   },
   extends: [
-    // "plugin:@typescript-eslint/eslint-recommended",
-    // "plugin:@typescript-eslint/recommended",
-    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "airbnb",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   settings: {
-    react: { version: "detect" },
+    react: { version: 'detect' },
   },
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
   },
 };
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const Application: React.SFC<{}> = () => (
-  <h1>Application</h1>
-);
+const Application: React.SFC<{}> = () => <h1>Applications too</h1>;
 
 render(<Application />, document.getElementById('root'));
